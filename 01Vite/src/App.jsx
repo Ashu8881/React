2ddad43680ef7efb3af9ee86c450e9d3dee9b3f4
Ashu8881 <you@example.com>
2024-03-01 10:32:37 +0530

    react projects

---
+++ 01Vite/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Chai from "./code";
+
+function App() {
+  return (
+    <div>
+      <h1>Ashutosh is coding</h1>
+      <Chai/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01basicsreact/src/chai.js
@@ -0,0 +1,4 @@
+function Chai(){
+    return <h2>Chai in progress</h2>
+}
+export default Chai
