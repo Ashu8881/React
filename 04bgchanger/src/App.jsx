88a2937d57c69a1d74590967f674971b806cc9b4
Ashu8881 <you@example.com>
2024-03-20 11:48:24 +0530

    counter

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -3,58 +3,29 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [color, setcolor] = useState("gray");
+  const colors = ["red", "green", "blue", "yellow", "orange", "pink", "purple"];
+  const [color, setColor] = useState("gray");
 
   return (
-    <div className=" h-screen  w-screen p-7 " style={{ backgroundColor: color }}>
-      <h2 className="font-bold text-xl  bg-black w-2/3">The BackgroundColor is {color}</h2>
+    <div
+      className=" h-screen  w-screen  "
+      style={{ backgroundColor: color }}
+    >
+      <h2 className="font-bold text-xl text-white bg-black w-2/3">
+        The BackgroundColor is {color}
+      </h2>
       <div className=" flex flex-wrap justify-center px-2 inset-x-0  fixed bottom-10">
         <div className=" flex flex-wrap justify-center   bg-cyan-700 rounded-full  shadow-lg gap-2">
-          <button
-            className=" bg-red-700 rounded-full p-2 m-3"
-            onClick={() => setcolor("red")}
-          >
-            Red
-          </button>
-          <button
-            className=" bg-green-700 rounded-full p-2 m-3"
-            onClick={() => setcolor("green")}
-          >
-            {" "}
-            Green
-          </button>
-          <button
-            className=" bg-blue-700 rounded-full p-2 m-3"
-            onClick={() => setcolor("blue")}
-          >
-            {" "}
-            Blue
-          </button>
-          <button
-            className=" bg-yellow-700 rounded-full p-2 m-3"
-            onClick={() => setcolor("yellow")}
-          >
-            {" "}
-            Yellow
-          </button>
-          <button
-            className=" bg-orange-700 rounded-full p-2 m-3"
-            onClick={() => setcolor("orange")}
-          >
-            Orange
-          </button>
-          <button
-            className=" bg-pink-700 rounded-full p-2 m-3"
-            onClick={() => setcolor("pink")}
-          >
-            Pink
-          </button>
-          <button
-            className=" bg-purple-700 rounded-full p-2 m-3"
-            onClick={() => setcolor("purple")}
-          >
-            Purpel
-          </button>
+          {colors.map((col, index) => (
+            <button
+              key={index}
+              className={`bg-${col}-700`}
+              onClick={() => setColor(col)}
+            >
+              {col.charAt(0).toUpperCase() + col.slice(1)}
+            </button>
+            
+          ))}
         </div>
       </div>
     </div>
--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,80 +1,83 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState, useRef } from "react";
 
 function App() {
   const [length, setlength] = useState(8);
   const [allowNum, setnumAllowed] = useState(false);
-  const [charallowed, setcharallowed] = useState(false);
+  const [charallowed, setcharAllowed] = useState(false);
   const [password, setpassword] = useState("");
-  const passwerdGenaerator = useCallback(
-    function () {
-      let pass = "";
-      let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      if (charallowed) str += "`{}][|!@#$%^&*(_+) ";
-      if (allowNum) str += "1234567890";
-      for (let i = 0; i <= length; i++) {
-        let char = Math.floor(Math.random() * str.length + 1);
-        pass = str.charAt(char);
-      }
-      setpassword(pass);
-    },
-    [length, allowNum, charallowed, setpassword]
-  );
+  const passwordref = useRef(null)
+  const passwerdGenaerator = useCallback(() => {
+    let pass = "";
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (charallowed) str += "`{}][|!@#$%^&*(_+) ";
+    if (allowNum) str += "1234567890";
+    for (let i = 0; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setpassword(pass);
+  }, [length, allowNum, charallowed, setpassword]);
 
+const copypasswordtoclipboard = useCallback(()=>{
+  passwordref.current?.select()
+  passwordref.current?.setSelectionRange(3,9)
+  window.navigator.clipboard.writeText(password)},[password])
+
+  useEffect(() => {
+    passwerdGenaerator();
+  }, [length, allowNum, charallowed, passwerdGenaerator]);
   return (
     <>
-      <h1>Password Generator </h1>
+      <div className="w-full shadow-sm rounded-lg items-center mx-auto py-4  ">
+        <h1 >Password Generator </h1>
 
-      <div className="w-full max-w-md mx-auto shadow-sm rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
-        test
-        <div className="flex  shadow rounded-lg overflow-hidden mb-4">
+        <div className="flex w-full  shadow rounded-lg overflow-hidden mb-4 bg-red-300 ">
           <input
             type="text"
             value={password}
             readOnly
-            placeholder="password"
-            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            className=" "
+            ref={passwordref}
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          <button  onClick={copypasswordtoclipboard}className="">
             Copy
           </button>
         </div>
-        <div className="flex text-sm gap-x-2">
+        <div className="flex text-md w-full gap-x-2">
           <input
+          
             type="range"
             name=""
             id=""
             min={8}
             max={20}
             value={length}
-            className="cursor-pointer"
+            className=" "
             onChange={(e) => {
               setlength(e.target.value);
             }}
           />
-          <label htmlFor="">Length ({length})</label> 
-          <div className=" flex items-center gap-x-1">
-       
-          <input
-            type="checkbox"
-            defaultChecked={allowNum}
-            onChange={() => {
-              setnumAllowed((prev) => !prev);
-            }}
-          /> <label htmlFor="">Number</label>
+          <label htmlFor="">Length ({length})</label>
+          <div className=" ">
+            <input
+              type="checkbox"
+              defaultChecked={allowNum}
+              onChange={() => {
+                setnumAllowed((prev) => !prev);
+              }}
+            />{" "}
+            <label htmlFor="">Number</label>
+            <input
+              type="checkbox"
+              defaultChecked={allowNum}
+              onChange={() => {
+                setcharAllowed((prev) => !prev);
+              }}
+            />{" "}
+            <label htmlFor="">Character</label>
+          </div>
         </div>
-        <div className=" flex items-center gap-x-1">
-       
-          <input
-            type="checkbox"
-            defaultChecked={allowNum}
-            onChange={() => {
-              setnumAllowed((prev) => !prev);
-            }}
-          /> <label htmlFor="">Character</label>
-        </div>
-        
-        </div>
-        
       </div>
     </>
   );
